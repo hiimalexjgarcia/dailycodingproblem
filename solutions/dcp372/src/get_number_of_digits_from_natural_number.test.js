b737a48f2b841f9d15a33bdbba4484153315cca1
Alex Garcia <hi.im.alex.j.garcia@gmail.com>
2020-01-18 19:54:38 -0700

    Starting solution for dcp372

---
+++ solutions/dcp372/src/get_number_of_digits_from_natural_number.test.js
@@ -0,0 +1,20 @@
+/*
+Daily Coding Problem: Problem #372 [Easy]
+
+Good morning! Here's your coding interview problem for today.
+
+This problem was asked by Amazon.
+
+Write a function that takes a natural number as input and returns the number of digits the input has.
+
+Constraint: don't use any loops.
+
+*/
+/* eslint-env jest */
+
+const getNumberOfDigitsFromNaturalNumber = require('./get_number_of_digits_from_natural_number')
+
+test('given number 234, return 3', () => {
+  expect(getNumberOfDigitsFromNatualNumber(234)).toBe(3)
+})
+
