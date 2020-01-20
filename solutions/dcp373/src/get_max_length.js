8e6b3ff69543cd3e731ad9454b3c5af0f4c3a38d
Alex Garcia <hi.im.alex.j.garcia@gmail.com>
2020-01-20 03:57:51 -0700

    Added solution for dcp373

---
+++ solutions/dcp373/src/get_max_length.js
@@ -0,0 +1,41 @@
+/*
+    Daily Coding Problem: Problem 373 [Hard]
+
+    Good morning! Here's your coding interview problem for today.
+
+    This problem was asked by Facebook.
+
+    Given a list of integers L, find the maximum length of a sequence of consecutive numbers that can be formed using elements from L.
+
+    For example, given L = [5, 2, 99, 3, 4, 1, 100], return 5 as we can build a sequence [1, 2, 3, 4, 5] which has length 5.
+*/
+
+function getMaxLength(l) {
+    if (l.length < 3)
+	return l.length
+
+    l.sort((a, b) => a-b)
+    l.push(Number.MIN_VALUE)
+
+    let previous = 0
+    let current = l.shift()
+    let length = 1
+    let maxLength = 1
+
+    do {
+	previous = current
+	current = l.shift()
+	if (previous === (current - 1))
+	    length++
+	else {
+	    if (length >= maxLength) {
+		maxLength = length
+		length = 1
+	    }
+	}
+    } while (l.length !== 0)
+    
+    return maxLength
+}
+
+module.exports = getMaxLength
---
+++ solutions/dcp373/src/get_max_length.test.js
@@ -0,0 +1,25 @@
+/*
+  Daily Coding Problem: Problem 373 [Hard]
+
+  Good morning! Here's your coding interview problem for today.
+
+  This problem was asked by Facebook.
+
+  Given a list of integers L, find the maximum length of a sequence of consecutive numbers that can be formed using elements from L.
+
+  For example, given L = [5, 2, 99, 3, 4, 1, 100], return 5 as we can build a sequence [1, 2, 3, 4, 5] which has length 5.
+*/
+
+const getMaxLength = require('./get_max_length')
+
+test('given [5, 2, 99, 3, 4, 1, 100], return 5', () => {
+    expect(getMaxLength([5, 2, 99, 3, 4, 1, 100])).toBe(5)
+})
+
+test('given [3, 2, 4, 11, 9, 10, 12], return 4', () => {
+    expect(getMaxLength([3, 2, 4, 11, 9, 10, 12])).toBe(4)
+})
+
+test('given [8, 3, 82, 38, 28, 4, 7, 9, -1, 0, 12, 15, 16, 1, 2, 10, 11], return 6', () => {
+    expect(getMaxLength([8, 3, 82, 38, 28, 4, 7, 9, -1, 0, 12, 15, 16, 1, 2, 10, 11])).toBe(6)
+})
