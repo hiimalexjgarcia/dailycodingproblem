71293c1620badf8dcdcc993bc2309b8105ec86e0
Alex Garcia <hi.im.alex.j.garcia@gmail.com>
2020-01-20 04:07:51 -0700

    linted code for dcp372

--- solutions/dcp372/src/get_number_of_digits_from_natural_number.test.js
+++ solutions/dcp372/src/get_number_of_digits_from_natural_number.test.js
@@ -1,13 +1,13 @@
 /*
-Daily Coding Problem: Problem #372 [Easy]
+  Daily Coding Problem: Problem 372 [Easy]
 
-Good morning! Here's your coding interview problem for today.
+  Good morning! Here's your coding interview problem for today.
 
-This problem was asked by Amazon.
+  This problem was asked by Amazon.
 
-Write a function that takes a natural number as input and returns the number of digits the input has.
+  Write a function that takes a natural number as input and returns the number of digits the input has.
 
-Constraint: don't use any loops.
+  Constraint: don't use any loops.
 
 */
 /* eslint-env jest */
